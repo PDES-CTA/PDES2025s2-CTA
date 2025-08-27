c2d70dbd142767b94d171ef4a99c5ec33f59d895
Nahuel Pacheco <nahuel.pacheco@protonmail.com>
2025-08-27 20:11:22 -0300

    feat: add user

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,21 +1,56 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+
+val hibernateValidatorVersion = "7.0.1.Final"
+val springdocVersion = "2.4.0"
+
 plugins {
-    kotlin("jvm") version "2.2.0"
+    war
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
+    kotlin("plugin.jpa") version "1.9.22"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "CTA"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocVersion")
+    implementation("org.hibernate.validator:hibernate-validator:$hibernateValidatorVersion")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "21"
+    }
 }
-kotlin {
-    jvmToolchain(24)
+
+tasks.withType<JavaCompile> {
+    options.release.set(21)
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
---
+++ backend/src/main/kotlin/CTA/UserRepository.kt
@@ -0,0 +1,9 @@
+package CTA
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<Usuario, Int> {
+
+}
