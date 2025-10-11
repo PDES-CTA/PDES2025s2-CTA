41d967856ce4177d87d2c38927a9f5061a1818dc
Nahuel Pacheco <nahuel.pacheco@protonmail.com>
2025-10-11 15:30:30 -0300

    rollback

--- backend/src/test/kotlin/cta/model/BuyerTest.kt
+++ backend/src/test/kotlin/cta/model/BuyerTest.kt
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 @DisplayName("Buyer Entity Tests")
-class BuyerTest { 
+class BuyerTest {
     private lateinit var buyer: Buyer
 
     @BeforeEach
